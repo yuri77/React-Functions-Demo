05daa208bf31de21b213293af9f624717c325559
Phil Smith <pbsmithtbn@gmail.com>
2019-07-23 20:19:05 -0500

    Initial commit

---
+++ src/components/Divide.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Divide(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Divide
---
+++ src/components/Subtract.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Subtract(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Subtract
